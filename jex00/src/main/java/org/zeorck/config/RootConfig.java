044cae32fb6f28ca44692aaf4214e52d6a37cc35
KyungHwa Park <kyunghwa.park123@gmail.com>
2019-01-09 00:48:08 +0900

    Tomcat 버전변경

--- jex00/src/main/java/org/zeorck/config/RootConfig.java
+++ jex00/src/main/java/org/zeorck/config/RootConfig.java
@@ -1,8 +1,12 @@
 package org.zeorck.config;
 
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
+// root-context.xml을 대신하는 설정 class (Java configuration)
+
 @Configuration
+@ComponentScan(basePackages = {"org.zeorck.sample"})
 public class RootConfig {
 
 }
